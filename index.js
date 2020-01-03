3c8e8c5eae2bdaa2182ae84b65ca6bdb74e15572
pointlessrapunzel <vladace9@gmail.com>
2020-01-03 21:51:11 +0500

    finish 3.4, implement delete route

--- index.js
+++ index.js
@@ -42,6 +42,13 @@ app.get('/api/persons/:id', (req, res) => {
   else res.status(404).end() 
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
