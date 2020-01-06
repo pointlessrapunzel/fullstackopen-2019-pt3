81384b912e9facdda3377768edd5470a16fb6a8d
pointlessrapunzel <vladace9@gmail.com>
2020-01-06 21:23:42 +0500

    finish ex3.15, implement deleting persons from DB

--- index.js
+++ index.js
@@ -68,10 +68,10 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
-
-  res.status(204).end()
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
 })
 
 app.post('/api/persons', (req, res) => {
